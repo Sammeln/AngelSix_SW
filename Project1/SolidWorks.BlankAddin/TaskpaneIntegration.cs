dcad67662711b49259603224a285432b3cddd55e
Sammeln <kirilln272@gmail.com>
2018-12-04 22:24:39 +0300

    1 commit

---
+++ Project1/SolidWorks.BlankAddin/TaskpaneIntegration.cs
@@ -0,0 +1,17 @@
+﻿using SolidWorks.Interop.swpublished;
+
+namespace SolidWorks.BlankAddin
+{
+    public class TaskpaneIntegration : ISwAddin
+    {
+        public bool ConnectToSW(object ThisSW, int Cookie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DisconnectFromSW()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
